a4f73deae396e06d4999b615f3a20f327d1865f6
LucasColas <lucas.colas13@gmail.com>
2021-04-05 06:16:53 +0200

    update

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import numpy as np
-from tensorflow.keras import models, layers, optimizers, metrics
+#from tensorflow.keras import models, layers, optimizers, metrics
 import os
+
+
+main_path = r'E:\Projets code\Fruit recognition'
+folders = os.listdir(main_path)
+
+for folder in folders:
+    print(folder)
+    files, extension = os.path.splitext(folder)
+    print(files, extension)
