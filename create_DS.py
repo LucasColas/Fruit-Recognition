951fbaae3457bffb702d10ea627bb2d007387849
LucasColas <lucas.colas13@gmail.com>
2021-04-08 06:43:23 +0200

    update

--- create_DS.py
+++ create_DS.py
@@ -6,10 +6,8 @@ import os
 main_path = r'E:\Projets code\DS Fruit recognition'
 folders = os.listdir(main_path)
 
-"""
+
 for folder in folders:
     #print(os.path.join(main_path, folder))
     files = os.listdir(os.path.join(main_path, folder))
     print(len(files))
-
-"""
