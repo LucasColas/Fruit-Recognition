2be4ec434b94e3103268601826b26139fe4441f3
LucasColas <lucas.colas13@gmail.com>
2021-04-08 07:23:59 +0200

    update

--- create_DS.py
+++ create_DS.py
@@ -8,21 +8,26 @@ folders = os.listdir(main_path)
 
 folders_name = ["Train", "Validation"]
 def get_data(path, folder):
-
+    data = []
     classes_path = os.path.join(path, folder)
     #print(classes_path)
     classes = os.listdir(classes_path)
     print(classes)
-    label = [0 for j in range(len(classes))]
+
 
     for classe in classes:
         path_images = os.path.join(classes_path, classe)
         images = os.listdir(path_images)
         #print(len(images))
+
         for image in images:
-            pass
+            label_one_hot = [0 for j in range(len(classes))]
+            img = cv2.imread(os.path.join(path_images,image))
+            img_resize = cv2.resize(img, (120,120))
+            data.append()
 
 
 
-X_train = get_data(main_path, folders_name[0])
+
+data_train = get_data(main_path, folders_name[0])
 X_Val = None
