11592d9a4f4aa323ae480d4f66795e10a51819c5
LucasColas <lucas.colas13@gmail.com>
2021-04-09 06:07:52 +0200

    update

--- NN_Xception.py
+++ NN_Xception.py
@@ -2,4 +2,4 @@ from tensorflow.keras.applications import Xception
 import numpy as np
 import matplotlib.pyplot as plt
 
-from create_ds import X_train, y_test, X_val, y_val, X_test, y_test
+from create_ds import X_train, y_train, X_val, y_val, X_test, y_test
