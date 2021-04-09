9ff288f8c9c11b84e502a34c7e935bb375501574
LucasColas <lucas.colas13@gmail.com>
2021-04-09 06:15:06 +0200

    update

--- NN_Xception.py
+++ NN_Xception.py
@@ -1,5 +1,16 @@
 from tensorflow.keras.applications import Xception
+from tensorflow.keras import models, layers, optimizers
 import numpy as np
 import matplotlib.pyplot as plt
 
-from create_ds import X_train, y_train, X_val, y_val, X_test, y_test
+from create_DS import X_train, y_train, X_val, y_val, X_test, y_test
+
+
+
+Xception_arch = Xception(include_top = False, input_shape=(100,100,3))
+
+model = models.Sequential()
+model.add(Xception_arch)
+model.add(layers.Flatten())
+model.add(layers.Dense(256))
+model.add(layers.Dense(23, activation="softmax"))
