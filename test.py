7c23b8f5dcb7cdf5ba29211ac419c081a36fe752
LucasColas <lucas.colas13@gmail.com>
2021-04-14 06:33:39 +0200

    update

--- test.py
+++ test.py
@@ -14,10 +14,19 @@ for classe in classes:
     path_class = os.path.join(test_path, classe)
     images = os.listdir(path_class)
     for image in images:
-        image = cv2.imread(os.path.join(path_class, image))
-        new_image = cv2.resize(image, (100,100))
-        test_images.append(image)
-        break
+        try:
+            image = cv2.imread(os.path.join(path_class, image))
+            rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            new_image = cv2.resize(image, (100,100))
+            plt.imshow(new_image)
+            plt.show()
+            test_images.append(new_image)
+            break
+
+        except Exception as e:
+            print("error", str(e))
 
 
 np_images = np.asarray(test_images).reshape(-1,100,100,3)
+print(np_images)
+print(np_images.shape)
