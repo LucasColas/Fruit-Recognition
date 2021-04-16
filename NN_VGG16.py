d9eb47969c7c71f8bcd8f00f59f659a860e2a965
LucasColas <lucas.colas13@gmail.com>
2021-04-16 06:16:07 +0200

    update

--- NN_VGG16.py
+++ NN_VGG16.py
@@ -11,16 +11,25 @@ main_path = r'E:\Projets code\Dataset Fruit recognition'
 folders_path_train = os.path.join(main_path, "Train")
 folders_path_val = os.path.join(main_path, "Validation")
 print("folders valid",folders_path_val)
-train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip = True)
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=42, brightness_range=(10,120), shear_range=0.2, zoom_range=0.2, horizontal_flip = True, vertical_flip = True)
 train_generator = train_datagen.flow_from_directory(folders_path_train, target_size=(100,100), batch_size=32, class_mode="categorical")
 valid_generator = train_datagen.flow_from_directory(folders_path_val, target_size=(100,100), batch_size=32, class_mode="categorical")
 
 for batch, label in train_generator:
-    print(batch.shape)
-    print(label)
+    count = 0
+    for index, image in enumerate(batch):
+        plt.imshow(image)
+        print(label[index])
+        plt.show()
+        count +=1
+        if count >= 5:
+            break
     break
+
+"""
 steps_per_epoch = train_generator.n//train_generator.batch_size
 steps_size_valid = valid_generator.n//valid_generator.batch_size
+
 
 vgg = VGG16(weights="imagenet", include_top=False, input_shape=(100,100,3))
 vgg.trainable = False
@@ -56,48 +65,4 @@ plt.title('Training and validation loss')
 plt.legend()
 
 plt.show()
-"""
-
-def one_hot(labels, dimension=15):
-    results = np.zeros((len(labels), dimension))
-
-    for i, label in enumerate(labels):
-        results[i, label] = 1
-
-    return results
-
-X = np.array(X_train, dtype="float32").reshape(-1, 100,100,3)
-#X //= 255
-Y = one_hot(y_train)
-print(Y)
-
-X_valid = np.array(X_val, dtype='float32').reshape(-1, 100, 100,3)
-#X_valid //= 255
-Y_valid = one_hot(y_val)
-
-X_enc = preprocess_input(X)
-X_valid_enc = preprocess_input(X_valid)
-
-
-Inception_arch = InceptionV3(include_top = False, input_shape=(100,100,3))
-print("Inception_arch : ", Inception_arch)
-model = models.Sequential()
-model.add(Inception_arch)
-print("model",model)
-print("Inception_arch", Inception_arch)
-model.add(layers.Flatten())
-model.add(layers.Dense(256))
-model.add(layers.Dense(15, activation="softmax"))
-model.summary()
-print("trainable weights", len(model.trainable_weights))
-Inception_arch.trainable = False
-print("trainable weights", len(model.trainable_weights))
-
-model.compile(optimizer=optimizers.RMSprop(lr=2e-5), loss="categorical_crossentropy", metrics=["acc"])
-
-print("X_enc : ",X_enc.shape, "X_val end : ",X_valid_enc.shape)
-print("Y : ", Y.shape, "Y val : ", Y_valid.shape)
-
-model.fit(X_enc, Y, batch_size=32, epochs=15, validation_data=(X_valid_enc, Y_valid))
-
 """
